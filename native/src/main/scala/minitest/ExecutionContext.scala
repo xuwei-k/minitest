01410d0d11c48fbfa028e254bd6aef2ce695b509
xuwei-k <6b656e6a69@gmail.com>
2018-06-29 15:36:45 +0900

    wip

--- native/src/main/scala/minitest/ExecutionContext.scala
+++ native/src/main/scala/minitest/ExecutionContext.scala
@@ -3,5 +3,5 @@ package minitest
 trait ExecutionContext
 
 object ExecutionContext {
-  implicit val global: ExecutionContext = new ExecutionContext{}
+  val global: ExecutionContext = new ExecutionContext{}
 }
